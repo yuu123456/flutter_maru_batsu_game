c653c0b703c7da9b5353f4b8f0f8abe1605d513d
yuu/green <yuu-akiyama@greensys.co.jp>
2023-08-21 16:52:45 +0900

    ユーザー名及びNPC設定画面作成

---
+++ lib/models/player.freezed.dart
@@ -0,0 +1,144 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'player.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+mixin _$Player {
+  bool get isNPC => throw _privateConstructorUsedError;
+  String get name => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $PlayerCopyWith<Player> get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $PlayerCopyWith<$Res> {
+  factory $PlayerCopyWith(Player value, $Res Function(Player) then) =
+      _$PlayerCopyWithImpl<$Res, Player>;
+  @useResult
+  $Res call({bool isNPC, String name});
+}
+
+/// @nodoc
+class _$PlayerCopyWithImpl<$Res, $Val extends Player>
+    implements $PlayerCopyWith<$Res> {
+  _$PlayerCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? isNPC = null,
+    Object? name = null,
+  }) {
+    return _then(_value.copyWith(
+      isNPC: null == isNPC
+          ? _value.isNPC
+          : isNPC // ignore: cast_nullable_to_non_nullable
+              as bool,
+      name: null == name
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$_PlayerCopyWith<$Res> implements $PlayerCopyWith<$Res> {
+  factory _$$_PlayerCopyWith(_$_Player value, $Res Function(_$_Player) then) =
+      __$$_PlayerCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call({bool isNPC, String name});
+}
+
+/// @nodoc
+class __$$_PlayerCopyWithImpl<$Res>
+    extends _$PlayerCopyWithImpl<$Res, _$_Player>
+    implements _$$_PlayerCopyWith<$Res> {
+  __$$_PlayerCopyWithImpl(_$_Player _value, $Res Function(_$_Player) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? isNPC = null,
+    Object? name = null,
+  }) {
+    return _then(_$_Player(
+      isNPC: null == isNPC
+          ? _value.isNPC
+          : isNPC // ignore: cast_nullable_to_non_nullable
+              as bool,
+      name: null == name
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_Player implements _Player {
+  _$_Player({required this.isNPC, required this.name});
+
+  @override
+  final bool isNPC;
+  @override
+  final String name;
+
+  @override
+  String toString() {
+    return 'Player(isNPC: $isNPC, name: $name)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$_Player &&
+            (identical(other.isNPC, isNPC) || other.isNPC == isNPC) &&
+            (identical(other.name, name) || other.name == name));
+  }
+
+  @override
+  int get hashCode => Object.hash(runtimeType, isNPC, name);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$_PlayerCopyWith<_$_Player> get copyWith =>
+      __$$_PlayerCopyWithImpl<_$_Player>(this, _$identity);
+}
+
+abstract class _Player implements Player {
+  factory _Player({required final bool isNPC, required final String name}) =
+      _$_Player;
+
+  @override
+  bool get isNPC;
+  @override
+  String get name;
+  @override
+  @JsonKey(ignore: true)
+  _$$_PlayerCopyWith<_$_Player> get copyWith =>
+      throw _privateConstructorUsedError;
+}
